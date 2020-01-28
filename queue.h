06c314667fa73031b82500d3ac524df97bb0307e
parker <parkergriep@yahoo.com>
2020-01-28 18:04:25 -0500

    add strqueue and make more tests so our teams implement exactly what we want

--- queue.h
+++ queue.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "object.h"
+#include "string.h"
 
 /**
  * @brief Represents a FIFO queue for containing objects.
@@ -12,13 +13,6 @@ class Queue : public Object {
      * 
      */
     Queue();
-    
-    /**
-     * @brief Constructs a new Queue object as a copy of the given queue.
-     * 
-     * @param q 
-     */
-    Queue(Queue& q);
 
     /**
      * @brief Destroys the Queue object.
@@ -67,4 +61,97 @@ class Queue : public Object {
      * @brief Removes all elements from the queue, deleting them.
      */
     void clear();
+
+    /**
+     * @brief Returns the hashcode of this Queue.
+     * 
+     * @return unsigned long 
+     */
+    virtual unsigned long hash();
+
+    /**
+     * @brief Determines whether this queue is equal to the
+     * given object.
+     * 
+     * @param o 
+     * @return bool
+     */
+    virtual bool equals(Object* o);
+};
+
+/**
+ * @brief Represents a FIFO queue for containing strings.
+ * 
+ */
+class StrQueue : public Object {
+  public:
+    /**
+     * @brief Constructs a new empty StrQueue object.
+     * 
+     */
+    StrQueue();
+
+    /**
+     * @brief Destroys the StrQueue object.
+     * This will delete all strings currently in the queue.
+     */
+    ~StrQueue();
+
+    /**
+     * @brief Adds the given string to the queue.
+     * 
+     * @param other 
+     */
+    void enqueue(String *other);
+
+    /**
+     * @brief Removes a string from the queue, returning
+     * nullptr if the queue is empty.
+     * 
+     * @return String* 
+     */
+    String* dequeue();
+
+    /**
+     * @brief Returns, but doesn't remove, the head of the queue.
+     * Returns nullptr if the queue is empty.
+     * 
+     * @return String* 
+     */
+    String* peek();
+
+    /**
+     * @brief Returns the number of elements in the queue.
+     * 
+     * @return unsigned long 
+     */
+    unsigned long size();
+
+    /**
+     * @brief Returns whether the queue contains any elements.
+     * 
+     * @return bool 
+     */
+    bool is_empty();
+
+    /**
+     * @brief Removes all elements from the queue, deleting them.
+     */
+    void clear();
+
+    /**
+     * @brief Returns the hashcode of this Queue.
+     * 
+     * @return unsigned long 
+     */
+    virtual unsigned long hash();
+
+    /**
+     * @brief Determines whether this queue is equal to the
+     * given object.
+     * 
+     * @param o 
+     * @return bool
+     */
+    virtual bool equals(Object* o);
 };
